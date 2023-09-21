a64044d90c43ac9f13bfbd12eecc3852a8704c2a
Ildi Dvorani <dvoranii@ildidev.com>
2023-09-21 16:56:40 -0400

    created project thumbnails

--- src/Components/ProjectsComponent/index.jsx
+++ src/Components/ProjectsComponent/index.jsx
@@ -1,14 +1,29 @@
 import "./styles.css";
 import TitleComponent from "../TitleComponent";
+import OGModalThumb from "/assets/modals/banner-img.webp";
+import CGLModalThumb from "/assets/modals/CGL-logo.png";
 
 function ProjectsComponent() {
   return (
-    <div>
+    <div id="projects-section" className={`projects--wrapper container`}>
       <TitleComponent
         className="projects-title"
         textContent={"My Projects"}
         classTitle="projects-title"
       ></TitleComponent>
+
+      <div className="projects--thumbnails__container-outer">
+        <div className="project_1-thumbnail--wrapper">
+          <div className="project_1-thumbnail">
+            <img src={OGModalThumb}></img>
+          </div>
+        </div>
+        <div className="project_2-thumbnail--wrapper">
+          <div className="project_2-thumbnail">
+            <img src={CGLModalThumb}></img>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
